a33c6f86f6163f69bbc63e417ddf6ca6188169df
demsasha4yt <demsasha4yt@yandex.ru>
2020-11-14 18:09:04 +0300

    norme errors.c

--- app/src/errors.c
+++ app/src/errors.c
@@ -6,18 +6,18 @@
 /*   By: bharrold <bharrold@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/14 17:11:29 by bharrold          #+#    #+#             */
-/*   Updated: 2020/11/14 17:11:31 by bharrold         ###   ########.fr       */
+/*   Updated: 2020/11/14 17:50:44 by bharrold         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ping.h"
 
-const char	*g_usage_str ="Usage: ping [-afhvq] [-i interval] [-t ttl] host\n\n\
-Usage: ping -h for help\n"
+const char	*g_usage = "Usage: ping [-afhvq] [-i interval] [-t ttl] host\n\n\
+Usage: ping -h for help\n";
 
 void		print_usage(void)
 {
-	dprintf(2, g_usage_str);
+	dprintf(2, g_usage);
 }
 
 void		print_error(int code)
